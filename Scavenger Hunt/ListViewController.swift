c435850d08847ccabb0c6d7d32b5649c9978f8ee
Apple <apple@Apples-MacBook-4.local>
2016-03-26 15:21:15 -0400

    C

---
+++ Scavenger Hunt/ListViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ListViewController.swift
+//  Scavenger Hunt
+//
+//  Created by Apple on 3/26/16.
+//  Copyright © 2016 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController : UITableViewController {
+    var itemsList = [
+        ScavengerHuntItem(name: "Cat"),
+        ScavengerHuntItem(name: "Bird"),
+        ScavengerHuntItem(name: "Brick")
+    ]
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemsList.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ListViewCell", forIndexPath: indexPath)
+        let item = itemsList[indexPath.row]
+        cell.textLabel?.text = item.name
+        return cell
+    }
+    
+    @IBAction func unwindToList (segue : UIStoryboardSegue) {
+        let addVC = segue.sourceViewController as! AddViewController
+        if let newItem = addVC.newItem {
+            itemsList += [newItem]
+            let indexPath = NSIndexPath(forRow: itemsList.count - 1, inSection: 0)
+            tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+        }
+    }
+}
---
+++ Scavenger Hunt/itemsManager.swift
@@ -0,0 +1,33 @@
+//
+//  itemsManager.swift
+//  Scavenger Hunt
+//
+//  Created by Apple on 3/26/16.
+//  Copyright © 2016 Apple. All rights reserved.
+//
+
+import Foundation
+
+class ItemsManager {
+    var itemsList = [ScavengerHuntItem]()
+    
+    func archivePath() -> String? {
+        let directoryList = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        if let documentsPath = directoryList.first {
+            return documentsPath + "/ScavengerHuntItems"
+        }
+        assertionFailure("Could not determine where to store the file.")
+        return nil
+    }
+
+    func save() {
+        if let theArchivePath = archivePath() {
+            if NSKeyedArchiver.archiveRootObject(itemsList, toFile: theArchivePath) {
+                print("Saved successfully")
+            } else {
+                assertionFailure("Could not save data tp \(theArchivePath)")
+            }
+        }
+    }
+    
+}
